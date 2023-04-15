46a899540a355187fee73bf9d7947de758270344
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-04-15 17:49:59 +0530

    Adding other methods of the Servlet in the class.

--- src/java/servelets/FirstServelet.java
+++ src/java/servelets/FirstServelet.java
@@ -5,12 +5,15 @@
  */
 package servelets;
 
+import java.io.IOException;
 import javax.servlet.*;
 /**
  *
  * @author Gyanendra-Yadav
  */
-public class FirstServelet {
+public class FirstServelet implements Servlet{
+
+    private ServletConfig config;
     
     //Adding lifecycle methods of the servelet
     
@@ -21,8 +24,27 @@ public class FirstServelet {
     }
     
     //this is the second phase of Servlet Life-Cycle 
-    public void service(ServletRequest req, ServletResponse res){  
+    public void service(ServletRequest req, ServletResponse res)throws ServletException, IOException{  
         System.out.println("Hey.....! On the second LifeCycle Phase of Servlet");
+    }
+    
+    //this is the 3rd method of LifeCycle of Servelet
+    public void destroy(){
+        System.out.println("Hey.......! Going to destroy the Servlet");
+    }
+    
+    //Another method of the servlet LifeCycle \
+    public String getServeletInfo(){
+        return "Hey.......! Returning serveletInfo";
+    }
+    
+    public ServletConfig getServletConfig(){
+        return this.config;
+    }
+
+    @Override
+    public String getServletInfo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
