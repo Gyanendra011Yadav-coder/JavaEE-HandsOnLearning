9bf675888b402c3f9158e0b207a23f544b410812
DESKTOP-FRL0M1V\Gyanendra-Yadav <gyanendra.yadav@deltatech.gg>
2023-04-16 12:55:57 +0530

    Now, we are going to implement Servlet using Generic Servlet Method. That already implements Servlet Interface.

---
+++ src/java/servelets/SecondServletUsingGenricServlet.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servelets;
+
+import java.io.IOException;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author Gyanendra-Yadav
+ */
+public class SecondServletUsingGenricServlet extends GenericServlet{
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
